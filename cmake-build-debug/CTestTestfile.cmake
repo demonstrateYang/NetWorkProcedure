5373c748d7b175792fa2812aecb1752b78aa4a5f
YangJunPeng <13652712647@163.com>
2023-09-18 13:05:15 +0800

    First Commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yangjunpeng/network-programing
+# Build directory: /Users/yangjunpeng/network-programing/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[main]=] "/Users/yangjunpeng/network-programing/cmake-build-debug/main")
+set_tests_properties([=[main]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/yangjunpeng/network-programing/CMakeLists.txt;16;add_test;/Users/yangjunpeng/network-programing/CMakeLists.txt;0;")
